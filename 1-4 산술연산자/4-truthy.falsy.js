ed18b9ee494a236849716fa08d1a1d454d05fa15
hee123hee <gmldnjsl5@naver.com>
2024-10-29 10:30:23 +0900

    241029

---
+++ 1-4 산술연산자/4-truthy.falsy.js
@@ -0,0 +1,10 @@
+if (false) console.log('참입니다1');
+if (0) console.log('참입니다2');
+if (null) console.log('참입니다3');
+if (undefined) console.log('참입니다4');
+if (NaN) console.log('참입니다5');
+if ('') console.log('참입니다6');
+if ('hello') console.log('참입니다7'); // true
+if (-100.999) console.log('참입니다8'); // true
+if ([1,2,3]) console.log('참입니다9'); // true
+if (function () { }) console.log('참입니다10'); // true
