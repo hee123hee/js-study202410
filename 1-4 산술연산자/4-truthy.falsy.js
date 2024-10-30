7c98a9c7588f76d094ec404b82359a1b77347d0a
hee123hee <gmldnjsl5@naver.com>
2024-10-30 15:52:58 +0900

    수업내용 업로드_241030

--- 1-4 산술연산자/4-truthy.falsy.js
+++ 1-4 산술연산자/4-truthy.falsy.js
@@ -1,3 +1,6 @@
+
+// truthy, falsy : js는 논리가 아닌 데이터를 논리로 표현할 수 있다.
+// 0, '', null, undefined, NaN - false로 취급한다. 나머지는 전부 true
 if (false) console.log('참입니다1');
 if (0) console.log('참입니다2');
 if (null) console.log('참입니다3');
--- 1-4 산술연산자/5-연습문제.js
+++ 1-4 산술연산자/5-연습문제.js
@@ -1,17 +1,34 @@
 let x = 10;
-console.log('초기 x:', x);  // 출력: 초기 x: 10
+let y = 3;
 
-x += 5;  // x = x + 5
-console.log('x += 5:', x);  // 출력: x += 5: 15
+console.log(x + y); // 13
+x++;
+console.log(x - y); // 7
+console.log(x * y); // 30
+console.log(x % y); // 1
+console.log(x ** 3); // 1000
 
-x -= 3;  // x = x - 3
-console.log('x -= 3:', x);  // 출력: x -= 3: 12
+console.log('===========');
 
-x *= 2;  // x = x * 2
-console.log('x *= 2:', x);  // 출력: x *= 2: 24
+let num1 = '100';
+let num2 = 100;
 
-x /= 4;  // x = x / 4
-console.log('x /= 4:', x);  // 출력: x /= 4: 6
+console.log(num1 == num2); // true
+console.log(num1 === num2); // false
+console.log(num1 != num2); // false
+console.log(num1 !== num2); // true
 
-x %= 5;  // x = x % 5
-console.log('x %= 5:', x);  // 출력: x %= 5: 1
+console.log('===========');
+
+let a = 5;
+let b = 10;
+             // t   &&    t  -> t
+let result1 = a > 3 && b < 15;
+             // t    ||   f  -> t
+let result2 = a === 5 || b === 20;
+//             !true -> false
+let result3 = !(a < b);
+
+console.log(result1); // true
+console.log(result2); // true
+console.log(result3); // false
