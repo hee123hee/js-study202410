f019785e58f1ef5f52e54f3e55c06ac67000d20f
hee123hee <gmldnjsl5@naver.com>
2025-03-21 17:46:07 +0900

    조건문, 반복문 학습

--- 1-6-반복분/9-continue.js
+++ 1-6-반복분/9-continue.js
@@ -1,7 +1,6 @@
 
-
-for (let i = 1; i <= 10; i++) {
-  if (i % 3 === 0) continue;
+for (i=1; i<=10; i++) {
+  if(i%3 ===0) continue;
   console.log(i);
 }
-console.log('반복문 종료!');
+console.log('반복문 종료!')
