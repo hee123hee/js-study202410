bfadbb6bf9baca0fa885d53feae0d61b04300bcb
hee123hee <gmldnjsl5@naver.com>
2025-03-24 09:34:35 +0900

    Revert "조건문, 반복문 학습"
    This reverts commit f019785e58f1ef5f52e54f3e55c06ac67000d20f.

--- 1-6-반복분/9-continue.js
+++ 1-6-반복분/9-continue.js
@@ -1,6 +1,7 @@
 
-for (i=1; i<=10; i++) {
-  if(i%3 ===0) continue;
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) continue;
   console.log(i);
 }
-console.log('반복문 종료!')
+console.log('반복문 종료!');
